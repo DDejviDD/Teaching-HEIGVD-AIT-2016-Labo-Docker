d174f169d482e7d4b1f7513834827dcb8143109c
Dejvid Muaremi <dejvid95@hotmail.com>
2019-01-05 00:51:18 +0100

    The script work like the demo.

--- provision.sh
+++ provision.sh
@@ -13,6 +13,10 @@ echo "************************  install docker  ************************"
 wget -qO- https://get.docker.com/ | sh
 usermod -aG docker vagrant
 
+# Configure the subnet heig
+echo "************************  Configure the subnet heig  **************"
+docker network create --driver bridge heig
+
 # Build the haproxy image
 echo "************************  build haproxy image  ************************"
 cd /vagrant/ha
@@ -23,14 +27,14 @@ echo "************************  build webapp image  ************************"
 cd /vagrant/webapp
 docker build -t softengheigvd/webapp .
 
+# Run load balancer
+echo "************************  run haproxy  ************************"
+docker rm -f ha 2>/dev/null || true
+docker run -d -p 80:80 -p 1936:1936 -p 9999:9999 --network heig --name ha softengheigvd/ha
+
 # Run two webapps
 echo "************************  run webapps  ************************"
 docker rm -f s1 2>/dev/null || true
 docker rm -f s2 2>/dev/null || true
-docker run -d --name s1 softengheigvd/webapp
-docker run -d --name s2 softengheigvd/webapp
-
-# Run load balancer
-echo "************************  run haproxy  ************************"
-docker rm -f ha 2>/dev/null || true
-docker run -d -p 80:80 -p 1936:1936 -p 9999:9999 --link s1 --link s2 --name ha softengheigvd/ha
+docker run -d --network heig --name s1 softengheigvd/webapp
+docker run -d --network heig --name s2 softengheigvd/webapp
